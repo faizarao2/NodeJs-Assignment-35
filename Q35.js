298189f905393add5dfa28fbb515d667fcb71496
Faiza Rao <159902088+faizarao2@users.noreply.github.com>
2024-02-23 20:15:07 +0500

    Add files via upload

---
+++ Q35.js
@@ -0,0 +1,30 @@
+// Q35. Animals: Think of at least three different animals that have a common characteristic. Store the names of these animals in a list, and then use a for loop to print out the name of each animal. • Modify your program to print a statement about each animal, such as A dog would make a great pet. • Add a line at the end of your program stating what these animals have in common. You could print a sentence such as Any of these animals would make a great pet!
+//array of animals
+var animals = ["Dog", "Cat", "Horse"];
+//name of each animal using a for loop
+console.log("Names of animals:");
+for (var _i = 0, animals_1 = animals; _i < animals_1.length; _i++) {
+    var animal = animals_1[_i];
+    console.log(animal);
+}
+// statement about each animal
+console.log("\nStatements about animals:");
+for (var _a = 0, animals_2 = animals; _a < animals_2.length; _a++) {
+    var animal = animals_2[_a];
+    switch (animal) {
+        case "Dog":
+            console.log("A dog would make a great pet.");
+            break;
+        case "Cat":
+            console.log("A cat can be an independent companion.");
+            break;
+        case "Horse":
+            console.log(" A Horse are majestic animals known for their strength, speed, and grace.");
+            break;
+        default:
+            console.log("This animal is not recognized.");
+            break;
+    }
+}
+//the common characteristic
+console.log("\nAny of these animals would make a great pet!");
